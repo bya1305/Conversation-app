dd45010920f71d8cddb513b9027cc391d6093ae1
Josh Avina <josh.avina78@gmail.com>
2016-08-21 18:50:23 -0700

    landing & devise user functionality

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+class WelcomeController < ApplicationController
+  def landing
+    if user_signed_in?
+      redirect_to '/home'
+    end
+  end
+
+  def home
+    @user = current_user
+  end
+end
