e3510dbe0255d6d6ccf2f08326df1d2c7e76b8bd
Josh Avina <josh.avina78@gmail.com>
2016-08-21 21:47:47 -0700

    configured conversation basic setup

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,30 @@
 class ConversationsController < ApplicationController
+  def index
+    @conversations = Conversation.all
+  end
+
+  def new
+    @conversation = Conversation.new
+  end
+
+  def create
+    @conversation = Conversation.create(conversation_params)
+    if @conversation.save
+      redirect_to conversation_path(@conversation)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @conversation = Conversation.find(params[:id])
+  end
+
+
+
+  private
+
+    def conversation_params
+      params.require(:conversation).permit(:title, :category)
+    end
 end
