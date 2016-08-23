c208a701848eb74992a1db4ccd7b528380e8c392
Joshua Avina <s3nbonzakura78@gmail.com>
2016-08-23 04:58:46 +0000

    got message form working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,14 +6,15 @@ class MessagesController < ApplicationController
   def create
     @conversation = Conversation.find(params[:conversation_id])
     @message = @conversation.messages.build(message_params)
+    @message.user = current_user
     if @message.save
-      redirect_to :back
+      redirect_to @conversation
     end
   end
 
   private
 
     def message_params
-      params.require(:message).permit(:content)
+      params.require(:message).permit(:content, :user_id)
     end
 end
